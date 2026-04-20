9d376735e214066cc6a26ef1e86629c0828994b1
Tzipora Hecht <tzpz550@gmail.com>
2026-04-20 12:48:22 +0300

    add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/resources/favicon_io/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/resources/favicon_io/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/resources/favicon_io/favicon-16x16.png" />
+        <link rel="icon" href="/resources/favicon_io/favicon.ico" />
+        <link rel="manifest" href="/resources/favicon_io/site.webmanifest" />
+      </Head>
       <body>
         <Main />
         <NextScript />
